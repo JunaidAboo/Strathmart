506437fee445170b81c363ea5af4b887afed9245
JunaidAboo <junaidaboo6@gmail.com>
2026-06-27 14:15:02 +0300

    Fix CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 from app import models
 from app.routers import users, products, cart, orders, ratings, contacts, auth
 
 app = FastAPI(title="StrathMart API")
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://strathmart-su.netlify.app",
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "null"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
