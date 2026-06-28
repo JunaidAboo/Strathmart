efb5a77de586d9bd26dbef8c7b1e8bb32b45f3cc
JunaidAboo <junaidaboo6@gmail.com>
2026-06-28 14:48:03 +0300

    Add Cloudflare URL to CORS

--- app/main.py
+++ app/main.py
@@ -10,11 +10,12 @@ app = FastAPI(title="StrathMart API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://strathmart-su.netlify.app",
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-        "null"
-    ],
+    "https://strathmart-su.netlify.app",
+    "https://strathmart-frontend.junaidaboo6.workers.dev",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "null"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
